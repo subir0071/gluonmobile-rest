3de1dd37d3e5a2bb9fd2e7f15da3bf351db76314
Subir_Ghosh <techguy007@gmail.com>
2018-01-21 01:06:22 +0530

    delete functionality

--- NotesAppMobile/src/main/java/com/gluonapplication/views/TernaryPresenter.java
+++ NotesAppMobile/src/main/java/com/gluonapplication/views/TernaryPresenter.java
@@ -1,11 +1,14 @@
 package com.gluonapplication.views;
 
 
+import java.util.Optional;
+
 import com.gluonapplication.GluonApplication;
 import com.gluonapplication.beans.NotesBean;
 import com.gluonapplication.beans.NotesBeanWrapper;
 import com.gluonhq.charm.glisten.animation.BounceInDownTransition;
 import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.control.Alert;
 import com.gluonhq.charm.glisten.control.AppBar;
 import com.gluonhq.charm.glisten.control.CharmListCell;
 import com.gluonhq.charm.glisten.control.CharmListView;
@@ -20,6 +23,10 @@ import com.gluonhq.connect.provider.RestClient;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.HBox;
 
 public class TernaryPresenter {
 
@@ -27,7 +34,7 @@ public class TernaryPresenter {
     private View ternaryView;
     
     @FXML
-    private CharmListView<String, Long> nameList;
+    private CharmListView<NotesBean, Long> nameList;
 
     public void initialize() {
     	ternaryView.setShowTransitionFactory(BounceInDownTransition::new);
@@ -65,21 +72,38 @@ public class TernaryPresenter {
     private void populateNotesListInUI(NotesBeanWrapper notesBeanWrapper){
     	ObservableList<String> notesListStr = FXCollections.observableArrayList();
     	//notesListStr.add(notesBeanWrapper.getNote()+" "+notesBeanWrapper.getDate());
-    	for(NotesBean notesBean : notesBeanWrapper.getNotesBeanList()){
+    	/*for(NotesBean notesBean : notesBeanWrapper.getNotesBeanList()){
     		
     		notesListStr.add(notesBean.getNote());
-    	}
-    	nameList.setItems(notesListStr);
-    	
-    	nameList.setCellFactory(p -> new CharmListCell<String>(){
-    	    @Override public void updateItem(String item, boolean empty) {
+    		nameList.setit(notesBean);
+    	}*/
+    	nameList.setItems(FXCollections.observableArrayList(notesBeanWrapper.getNotesBeanList()));
+    	//nameList.seth
+    	nameList.setCellFactory(p -> new CharmListCell<NotesBean>(){
+    	    @Override public void updateItem(NotesBean item, boolean empty) {
     	        super.updateItem(item, empty); 
     	 
     	        if(item != null && !empty){
+    	        	HBox buttons = new  HBox();
+    	        	Button editBtn = MaterialDesignIcon.EDIT.button();
+    	        	editBtn.setId(item.getId());
+    	        	editBtn.setOnAction(p -> {
+    	        		p.getSource();
+    	        		System.out.println("Edit Pressed"+p);
+    	        		editNote(((Button)p.getSource()).getId());
+    	        		});
+    	        	Button delBtn = MaterialDesignIcon.DELETE.button();
+    	        	delBtn.setId(item.getId());
+    	        	delBtn.setOnAction(e -> {
+    	        		deleteNote(((Button)e.getSource()).getId());
+    	        	});
+    	        	buttons.getChildren().addAll(editBtn, delBtn);
+    	        	
     	            ListTile tile = new ListTile();
-    	            tile.setPrimaryGraphic(MaterialDesignIcon.EDIT.graphic());
-    	           // tile.textProperty().addAll(item);
-    	            setText(item);
+    	            tile.setId(item.getId());
+    	            tile.textProperty().add(item.getNote());
+    	            tile.setSecondaryGraphic(buttons);
+    	            setText(null);
     	            setGraphic(tile);
     	        } else {
     	            setText(null);
@@ -88,6 +112,41 @@ public class TernaryPresenter {
     	    }
     	});
     	
+    }
+    
+    public void editNote(String noteId){
+    	System.out.println("into Edit Note method");
+    	MobileApplication.getInstance().showMessage("In edit");
+    }
+    
+    public void deleteNote(String noteId){
+    	System.out.println("into delete note method");
+    	Alert alert = new Alert(AlertType.CONFIRMATION, "Confirm Note Deletion!");
+    	Optional<ButtonType> ans =alert.showAndWait();
+    	ButtonType btnType = ans.orElse(null);
+    	String txt = btnType.getText();
+    	 if (txt.equalsIgnoreCase("OK"))	{
+    		 deleteNotesRestClient(noteId);
+    		 MobileApplication.getInstance().showMessage("Note Successfully Deleted");
+    		 return;
+    	 } else {
+    		 MobileApplication.getInstance().showMessage("Note not Deleted");
+    	 }
+    }
+    
+    private void deleteNotesRestClient(String noteId){
+    	RestClient restClient = RestClient.create()
+				.host("http://localhost:8090")
+				.path("/note/delete")
+				.header("Content-Type", "application/json")
+				//.contentType("application/json")
+				.header("Accept", "application/json")
+				.method("POST");
+    	
+    	NotesBean chicken = new NotesBean();
+		chicken.setId(noteId);
+		GluonObservableObject<NotesBean> o = DataProvider.storeObject(chicken,
+				restClient.createObjectDataWriter(NotesBean.class));
     }
     
     public void addTextToList() {
